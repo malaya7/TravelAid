ec5901e18486919e6323a0280e24bd9e296b0b47
alber <alber@Fire-Canon>
2018-05-14 14:34:02 -0700

    last Scene

---
+++ TravelAid/src/edu/orangecoastcollege/view/EmailSenderView.java
@@ -0,0 +1,17 @@
+package edu.orangecoastcollege.view;
+
+import javafx.application.Application;
+
+import javafx.stage.Stage;
+
+public class EmailSenderView extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+
+
+
+	}
+
+
+}
